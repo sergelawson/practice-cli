5181a0fe3eca71f4e0298e30a021c3df17b91b23
Serge Lawson <iamsergelawson@gmail.com>
2023-08-18 01:03:53 +0000

    Add arguments and options

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
+import { Command } from "commander";
 
-import fs from "fs";
-const test = () => {
-  return "Cello";
+const test = (name: string) => {
+  return `${name} welcome to Practice CLI!`;
 };
+// Declare my program
+const program = new Command();
 
-console.log(test());
+// Add actions to my CLI
+program
+  .argument("<string>", "String to log")
+  .option("-c ,--capitalize", "Capitalize the string")
+  .action((message: string, opt: { capitalize: boolean }) => {
+    if (opt.capitalize) {
+      console.log(test(message.toUpperCase()));
+      return;
+    }
+    console.log(test(message));
+  });
+
+// Execute the program with the given argument
+
+program.parse(process.argv);

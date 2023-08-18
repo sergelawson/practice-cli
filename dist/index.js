2c3b2d5b577711608763cdae85bf926dae1cff2e
Serge Lawson <iamsergelawson@gmail.com>
2023-08-18 00:09:51 +0000

    build setup

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test = () => {
+    return "Cello";
+};
+console.log(test());
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Welcome to Practice CLI!");
